211b282e1661316311dc84df1d2c9edb75818e3d
Yulia Zaborska <zaborska.yulia@gmail.com>
2019-04-12 15:10:17 +0300

    cart

--- js/index.js
+++ js/index.js
@@ -10,7 +10,7 @@ fetch("./json/best-deals.json")
         <div class="paris-tour" style="background-image: url(${oneDeal.photo});background-size: cover;">
           <p class="header-tour">${oneDeal.text}</p>
           <p class="header-tour">$${oneDeal.price}</p>
-          <button class="add-to-cart-btn" data-text="${oneDeal.text}" data-price="${oneDeal.price}">
+          <button class="add-to-cart-btn">
             <i class="fas fa-shopping-cart"></i>
             <div class="v-line"></div>
             Add to Cart
@@ -18,30 +18,7 @@ fetch("./json/best-deals.json")
         </div>
       `;
     });
-
-    // buttons are ready
-    attachEventsToCartBtns();
   });
-
-function attachEventsToCartBtns() {
-  const btns = document.querySelectorAll('.add-to-cart-btn');
-
-  btns.forEach(function (btn) {
-    btn.addEventListener('click', addToCart);
-  });
-}
-
-function addToCart(event) {
-  const cartItemsEl = document.querySelector('#cart-items');
-  const clickedElData = event.target.dataset;
-
-  cartItemsEl.innerHTML += `<tr>
-                <td>${clickedElData.text}</td>
-                <td>$${clickedElData.price}</td>
-              </tr>`;
-
-}
-
 
 // Carousel
 $('.carousel').carousel({
